3280fdec6e7227b1932e4f3f45098e90b150ff9f
QOO <p61402@gmail.com>
2018-09-28 21:57:18 +0800

    Implement function q_insert_head

--- queue.c
+++ queue.c
@@ -60,16 +60,29 @@ void q_free(queue_t *q)
  */
 bool q_insert_head(queue_t *q, char *s)
 {
-    list_ele_t *newh;
-    /* What should you do if the q is NULL? */
-    newh = malloc(sizeof(list_ele_t));
-    /* Don't forget to allocate space for the string and copy it */
-    /* What if either call to malloc returns NULL? */
+    if (!q)
+        return false;
+
+    list_ele_t *newh = malloc(sizeof(list_ele_t));
+    if (!newh)
+        return false;
+
+    newh->value = malloc(strlen(s) + 1);
+    if (!newh->value) {
+        free(newh);
+        return false;
+    }
+
+    strcpy(newh->value, s);
+
     newh->next = q->head;
     q->head = newh;
+    if (q->size == 0)
+        q->tail = q->head;
+    q->size++;
+
     return true;
 }
-
 
 /*
   Attempt to insert element at tail of queue.
