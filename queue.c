3eea241d849198e50276e9ba5ad8808e589c96d4
QOO <p61402@gmail.com>
2018-09-28 21:24:17 +0800

    Implement function q_new

--- queue.c
+++ queue.c
@@ -25,8 +25,13 @@
 queue_t *q_new()
 {
     queue_t *q = malloc(sizeof(queue_t));
-    /* What if malloc returned NULL? */
-    q->head = NULL;
+
+    if (q) {
+        q->head = NULL;
+        q->tail = NULL;
+        q->size = 0;
+    }
+
     return q;
 }
 
